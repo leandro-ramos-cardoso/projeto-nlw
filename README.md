0c029dc30f1c68e826a0417091fb3aabd1337b22
Leandro Ramos Cardoso <lrcardoso.jp@gmail.com>
2025-07-09 19:12:29 -0300

    add-agenteIA

--- README.md
+++ README.md
@@ -103,4 +103,24 @@ A partir disso, o HTML é impactado visualmente.
 - Application
 - Programming
 - Interface
-- JSON: Javascript Object Notation
+- JSON: Javascript Object Notation **Notation**
+  
+# CDN
+- Content
+- Delivery
+- Network
+
+# LLM
+- Large
+- Language
+- Model
+- Gemini, OpenAI (chatGPT), Anthropic (Claude) ...
+  
+# Agentes AI
+- Tools (ferramentas): permitem uso de código, ou apps, ou qualquer informação extra.
+- Melhor contexto
+
+# Engenharia de prompt
+- One shot: uma única pergunta sem muito contexto
+- Few shot: é apresentado exemplos do que se espera
+- Chain of Thought: Cadeia de pensamentos para a IA responder gradativamente conforme a instrução.
--- script.js
+++ script.js
@@ -5,22 +5,76 @@ const askButton = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
+const markdownToHTML = (text) => {
+    const converter = new showdown.Converter()
+    return converter.makeHtml(text)
+}
+
 // AIzaSyAEN5uzV77my3VFXy1jwPjaIkNGI3bKeYM
 const perguntarAI = async (question, game, apiKey) => {
     const model = "gemini-2.5-flash"
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
     const pergunta = `
+        ## Especialidade
+        Voce é um especialista assistente de meta para o jogo ${game}
+
+        ## Tarefa
+        Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, build e dicas
+
+        ## Regras
+        - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+        - Se a pergunta não está relacionada ao jogo, responda com 'Essa pergunta não está relacionada ao jogo'
+        - Considere a data atual ${new Date().toLocaleDateString()}
+        - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+        - Nunca responda itens que você não tenha certeza de que existe no patch atual.
+
+        ## Resposta
+        - Economize na resposta, seja direto e responda no máximo 500 caracteres 
+        - Responda em markdown
+        - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+
+
+        ## Exemplo de resposta
+        pergunta do usuário: Melhor build rengar jungle
+        resposta: A build mais atual é: \n\n **Itens:**\n\n coloque os itens aqui.\n\n**Runas:**\n\nexemplo de runas\n\n
+
+
+        ---
+        Aqui está a pergunta do usuário: ${question}
     `
-    const contents = [{}]
+
+    const contents = [{
+        role: "user",
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    const tools = [{
+        google_search: {}
+    }]
+
+    // chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            contents,
+            tools
+        })
+    })
+
+    const data = await response.json()
+    return data.candidates[0].content.parts[0].text
 }
 
-const enviarFormulario = (event) => {
+const enviarFormulario = async (event) => {
     event.preventDefault()
     const apiKey = apiKeyInput.value
     const game = gameSelect.value
     const question = questionInput.value
-
-    console.log({apiKey, game, question})
 
     if(apiKey == '' || game == '' || question == '') {
         alert('Por favor preencha todos os campos')
@@ -32,8 +86,9 @@ const enviarFormulario = (event) => {
     askButton.classList.add('loading')
 
     try {
-        // Perguntar para a IA
-        perguntarAI(question, game, apiKey)
+        const text = await perguntarAI(question, game, apiKey)
+        aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text)
+        aiResponse.classList.remove('hidden')
     } catch(error) {
         console.log('Erro: ', error)
     } finally {
